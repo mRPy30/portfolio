ae14f19dacfd35bdc76815324e3459501a979774
Janvier Erickson <janviererickson.araque@cvsu.edu.ph>
2024-02-25 00:18:07 +0800

    porfolio

---
+++ home.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="short icon" href="logo.png" type="x-icon">
+    <title>
+        <?php echo "My Portfolio"; ?>
+    </title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <h1>hello world!</h1>
+    <p>This is ja</p>
+</body>
+</html>
